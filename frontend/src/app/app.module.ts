a9d145ade2633db37b50d64ceca34a4670746530
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-20 19:53:17 -0600

    created product-list

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductService } from './services/product.service';
+
+@NgModule({
+  declarations: [AppComponent, ProductListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [ProductService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
