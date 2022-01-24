b032a6ce85c76e5c3214b175e974a434608007d5
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-24 09:21:05 -0600

    added: shared module and routing feature

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { CheckoutSectionComponent } from './components/checkout-section/checkout-section.component';
+import { CategoriesPageComponent } from './pages/categories-page/categories-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'categories', component: CategoriesPageComponent },
+  { path: 'checkout', component: CheckoutSectionComponent },
+  { path: 'product-detail', component: ProductDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Tobi Shop';
+  title = 'Tobi';
+  showCart = false;
+
+  changeDisplay = (status: boolean) => {
+    this.showCart = status;
+    console.log(status);
+  };
 }
