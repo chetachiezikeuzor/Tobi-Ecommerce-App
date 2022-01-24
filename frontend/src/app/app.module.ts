71b1266e5f97d241cc828e6896ffb086bf4a03eb
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-24 09:16:08 -0600

    added: custom template pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,9 +16,13 @@ import { MainHeroComponent } from './components/main-hero/main-hero.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ShoppingCartDetailsComponent } from './components/shopping-cart-details/shopping-cart-details.component';
+import { CategoriesPageComponent } from './pages/categories-page/categories-page.component';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, AboutSectionComponent, CallToActionComponent, CategoriesGridComponent, CategoriesSectionComponent, CheckoutSectionComponent, FeaturedProductComponent, FooterSectionComponent, MainHeroComponent, NavigationBarComponent, ProductDetailComponent, ShoppingCartDetailsComponent],
+  declarations: [AppComponent, ProductListComponent, AboutSectionComponent, CallToActionComponent, CategoriesGridComponent, CategoriesSectionComponent, CheckoutSectionComponent, FeaturedProductComponent, FooterSectionComponent, MainHeroComponent, NavigationBarComponent, ProductDetailComponent, ShoppingCartDetailsComponent, CategoriesPageComponent, CheckoutPageComponent, HomePageComponent, ProductDetailPageComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [ProductService],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/pages/categories-page/categories-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-page',
+  templateUrl: './categories-page.component.html',
+  styleUrls: ['./categories-page.component.css']
+})
+export class CategoriesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
