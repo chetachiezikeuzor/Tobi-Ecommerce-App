bf1a2d11b00c83445415e66f2cd65a64669746cf
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-24 09:16:47 -0600

    added: custom styling and HTML elements to pages

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkout-page',
   templateUrl: './checkout-page.component.html',
-  styleUrls: ['./checkout-page.component.css']
+  styleUrls: ['./checkout-page.component.css'],
 })
 export class CheckoutPageComponent implements OnInit {
+  showCart = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  changeDisplay = (status: boolean) => {
+    this.showCart = status;
+    console.log(status);
+  };
 }
