dde33a3b02390474c3f083b2f208952ac1308590
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-26 00:41:15 -0600

    added: addToCart function for testing

--- frontend/src/app/components/shopping-grid/shopping-grid.component.ts
+++ frontend/src/app/components/shopping-grid/shopping-grid.component.ts
@@ -105,4 +105,8 @@ export class ShoppingGridComponent implements OnInit {
       this.theTotalElements = data.page.totalElements;
     };
   }
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart: ${theProduct.name}: $${theProduct.unitPrice}`);
+  }
 }
