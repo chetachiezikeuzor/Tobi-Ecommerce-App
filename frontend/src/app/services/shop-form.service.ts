f893ac5d73a381967f4438069504efcf2a281c85
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-28 14:50:08 -0600

    added: form methods

---
+++ frontend/src/app/services/shop-form.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShopFormService {
+  constructor() {}
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+    let data: number[] = [];
+
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+      data.push(theMonth);
+    }
+    return of(data);
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+    let data: number[] = [];
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
+      data.push(theYear);
+    }
+    return of(data);
+  }
+}
