b2242b5ea5de5e4b221c361a5889c6dc655f8d2e
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-28 14:50:17 -0600

    added: form methods

--- src/app/components/checkout-section/checkout-section.component.ts
+++ src/app/components/checkout-section/checkout-section.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
+import { ShopFormService } from 'src/app/services/shop-form.service';
 
 @Component({
   selector: 'app-checkout-section',
@@ -11,7 +14,19 @@ export class CheckoutSectionComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
-  constructor(private formBuilder: FormBuilder) {}
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+  states: State[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private shopFormService: ShopFormService
+  ) {}
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -42,6 +57,37 @@ export class CheckoutSectionComponent implements OnInit {
         expirationMonth: [''],
         expirationYear: [''],
       }),
+    });
+
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log('start m: ' + startMonth);
+
+    this.shopFormService.getCreditCardYears().subscribe((data) => {
+      console.log("Retrieved credit card y's: " + JSON.stringify(data));
+      this.creditCardYears = data;
+    });
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe((data) => {
+      console.log("Retrieved credit card m's: " + JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+
+    this.shopFormService.getCountries().subscribe((data) => {
+      this.countries = data;
+    });
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup?.value.country.code;
+
+    this.shopFormService.getStates(countryCode).subscribe((data) => {
+      if (formGroupName === 'shippingAddress') {
+        this.shippingAddressStates = data;
+      } else {
+        this.billingAddressStates = data;
+      }
+      formGroup?.get('state')?.setValue(data[0]);
     });
   }
 
