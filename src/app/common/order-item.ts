8ff72b305772d0a5cb54c0f4fd50fced06a86f9f
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-02-01 12:25:55 -0600

    added: config for checkout service

--- src/app/common/order-item.ts
+++ src/app/common/order-item.ts
@@ -3,12 +3,12 @@ import { CartItem } from './cart-item';
 export class OrderItem {
   imageUrl?: string;
   unitPrice?: number;
-  quanity?: number;
+  quantity?: number;
   productId?: number;
 
   constructor(cartItem: CartItem) {
     this.imageUrl = cartItem.imageUrl;
-    this.quanity = cartItem.quantity;
+    this.quantity = cartItem.quantity;
     this.unitPrice = cartItem.unitPrice;
     this.productId = cartItem.id;
   }
