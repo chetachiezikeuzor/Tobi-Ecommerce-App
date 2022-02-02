c02606093c75fa9021c10c58c7de01a8ff0d86a2
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-02-01 20:55:58 -0600

    added: configurations for cors mapping

--- src/main/java/com/tobi/ecommerce/configurations/TobiDataRestConfiguration.java
+++ src/main/java/com/tobi/ecommerce/configurations/TobiDataRestConfiguration.java
@@ -5,10 +5,12 @@ import com.tobi.ecommerce.models.Product;
 import com.tobi.ecommerce.models.ProductCategory;
 import com.tobi.ecommerce.models.State;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
@@ -19,6 +21,9 @@ import java.util.Set;
 @Configuration
 public class TobiDataRestConfiguration implements RepositoryRestConfigurer {
 
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
+
     private EntityManager entityManager;
 
     @Autowired
@@ -27,7 +32,7 @@ public class TobiDataRestConfiguration implements RepositoryRestConfigurer {
     }
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
@@ -38,6 +43,9 @@ public class TobiDataRestConfiguration implements RepositoryRestConfigurer {
         disableHttpMethods(State.class, config, theUnsupportedActions);
 
         exposeIds(config);
+
+        // configure cors mapping
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
--- src/main/java/com/tobi/ecommerce/repositories/ProductCategoryRepository.java
+++ src/main/java/com/tobi/ecommerce/repositories/ProductCategoryRepository.java
@@ -3,9 +3,7 @@ package com.tobi.ecommerce.repositories;
 import com.tobi.ecommerce.models.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategories", path = "product-categories")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
