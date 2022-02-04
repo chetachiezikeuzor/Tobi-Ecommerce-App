ff3c3f0d10ccd1a1fa27f321a62b0a4359080cf9
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-02-04 13:49:40 -0600

    update: style changes and security config

--- src/main/java/com/tobi/ecommerce/configurations/SecurityConfiguration.java
+++ src/main/java/com/tobi/ecommerce/configurations/SecurityConfiguration.java
@@ -7,9 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // protect our /api/orders endpoint
+
         http.authorizeRequests()
                 .antMatchers("/api/orders/**")
                 .authenticated()
