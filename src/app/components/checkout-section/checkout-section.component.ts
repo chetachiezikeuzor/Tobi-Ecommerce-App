2fe322be8a0238c897dc26372d35ff17e6007332
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-02-15 14:13:24 -0600

    added: browser reload bug fix for checkout

--- src/app/components/checkout-section/checkout-section.component.ts
+++ src/app/components/checkout-section/checkout-section.component.ts
@@ -367,6 +367,7 @@ export class CheckoutSectionComponent implements OnInit {
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
 
     this.checkoutFormGroup.reset();
 
