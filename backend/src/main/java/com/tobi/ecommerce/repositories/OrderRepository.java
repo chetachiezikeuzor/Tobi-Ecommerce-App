68da78fcdfbd285dbb0ddb4870348b9fd7da8495
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-02-21 23:45:55 -0600

    added: caching to order data rest
    Signed-off-by: Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>

--- backend/src/main/java/com/tobi/ecommerce/repositories/OrderRepository.java
+++ backend/src/main/java/com/tobi/ecommerce/repositories/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.tobi.ecommerce.repositories;
 
 import com.tobi.ecommerce.models.Order;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @CacheEvict(value="thirty-second-cache", key="'orderByCustomerEmailOrderByDateCreatedDesc'+#email", beforeInvocation = true)
+    @Cacheable(value="thirty-second-cache", key="'orderByCustomerEmailOrderByDateCreatedDesc'+#email")
     Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
 }
