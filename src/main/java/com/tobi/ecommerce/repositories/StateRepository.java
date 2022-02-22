7031f0d1a6e4d30f00c53385b4aa95aa5be46aa6
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-02-21 23:44:15 -0600

    added: caching to state data rest
    Signed-off-by: Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>

--- src/main/java/com/tobi/ecommerce/repositories/StateRepository.java
+++ src/main/java/com/tobi/ecommerce/repositories/StateRepository.java
@@ -1,6 +1,8 @@
 package com.tobi.ecommerce.repositories;
 
 import com.tobi.ecommerce.models.State;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,5 +11,8 @@ import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "states", path = "states")
 public interface StateRepository extends JpaRepository<State, Integer> {
+
+    @CacheEvict(value="thirty-second-cache", key="'stateByCountryCode'+#code", beforeInvocation = true)
+    @Cacheable(value="thirty-second-cache", key="'stateByCountryCode'+#code")
     List<State> findByCountryCode(@Param("code") String code);
 }
