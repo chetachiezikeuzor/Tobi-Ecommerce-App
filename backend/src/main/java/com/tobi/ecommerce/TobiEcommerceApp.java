ea597585dac97e7cf90f3404589d975ee600320b
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-03-03 13:31:21 -0600

    removed: Multiple app entries
    Signed-off-by: Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>

--- backend/src/main/java/com/tobi/ecommerce/TobiEcommerceApp.java
+++ backend/src/main/java/com/tobi/ecommerce/TobiEcommerceApp.java
@@ -2,8 +2,11 @@ package com.tobi.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+// enables the cache management capability
+@EnableCaching
 public class TobiEcommerceApp  {
 
 	public static void main(String[] args) {
