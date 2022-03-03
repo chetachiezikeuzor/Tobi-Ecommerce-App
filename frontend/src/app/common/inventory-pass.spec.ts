8fd14ae71bf3831d355b9118965a25cca3f80d07
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-03-03 09:35:42 -0600

    added: inventory management implementation on frontend
    Signed-off-by: Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>

---
+++ frontend/src/app/common/inventory-pass.spec.ts
@@ -0,0 +1,7 @@
+import { InventoryPass } from './inventory-pass';
+
+describe('InventoryPass', () => {
+  it('should create an instance', () => {
+    expect(new InventoryPass()).toBeTruthy();
+  });
+});
