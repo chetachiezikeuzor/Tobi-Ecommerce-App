931f743eb4c7d58f56961ecdb92d75ce2f9055c8
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-03-03 09:36:40 -0600

    added: caching annotations
    Signed-off-by: Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>

--- src/main/java/com/tobi/ecommerce/App.java
+++ src/main/java/com/tobi/ecommerce/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-//enables the cache management capability
+// enables the cache management capability
 @EnableCaching
 public class App  {
 
--- src/main/java/com/tobi/ecommerce/configurations/CachingConfiguration.java
+++ src/main/java/com/tobi/ecommerce/configurations/CachingConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import net.sf.ehcache.config.CacheConfiguration;
 
 @Configuration
-//enable caching
+// enable caching
 @EnableCaching
 public class CachingConfiguration extends CachingConfigurerSupport {
 
