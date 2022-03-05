2b817ee467a69107757090b600e86c846a56bdd4
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-03-04 19:44:35 -0600

    fixed: spelling mistake

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/frontnd/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
 });
 app.listen(process.env.PORT || 8080);
