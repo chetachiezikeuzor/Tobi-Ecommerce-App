b60d5596895fa54afd4f80f140a475062beb8ad8
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-03-05 20:25:14 -0600

    remove: build flags

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/<app-name>"));
+app.use(express.static(__dirname + "/dist/frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<app-name>/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
 });
 app.listen(process.env.PORT || 8080);
