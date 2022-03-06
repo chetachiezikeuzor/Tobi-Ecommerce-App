5be185ef6883e22ab97f440b8d8569a0bd3e01ec
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-03-06 00:42:48 -0600

    change: Start without SSL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  tobiApiUrl: 'https://tobi-ecommerce-app.herokuapp.com/api',
+  stripePublishableKey:
+    'pk_test_51H4IXzIcCHriQoIn1JSV38ceQTWiq2CrFyTZnqLHSPeuHReLvCeqCXvGg4lf7OrEqaSr0hBrVtYwxpV4BeiwsyxV00tYD23I3j',
 };
