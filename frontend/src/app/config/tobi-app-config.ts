e1219cf14c4df04bad14a4f411124ff001d2a486
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-03-06 14:30:10 -0600

    change: Redirect uri

--- frontend/src/app/config/tobi-app-config.ts
+++ frontend/src/app/config/tobi-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa3s3tzd8fPb8nMv5d7', // public identifier
     issuer: 'https://dev-25748741.okta.com/oauth2/default', // issuer of tokens
-    redirectUri: 'https://tobi-ecommerce-app.herokuapp.com/login/callback', // where we send the user once they login
+    redirectUri: window.location.origin, // where we send the user once they login
     scopes: ['openid', 'profile', 'email'], // access to information about the user
   },
 };
