57ec1706a38a000048cf8fc4531f67a79e4d7cf3
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-03-06 15:07:12 -0600

    added: Port details

--- backend/src/main/java/com/tobi/ecommerce/repositories/InventoryItemRepository.java
+++ backend/src/main/java/com/tobi/ecommerce/repositories/InventoryItemRepository.java
@@ -2,6 +2,7 @@ package com.tobi.ecommerce.repositories;
 
 import com.tobi.ecommerce.models.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface InventoryItemRepository extends JpaRepository<InventoryItem, Integer> {
 }
